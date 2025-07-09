88d6604308ff7008a4f99e0c277051414ba65c9b
Umesh Joshi <uj19343@gmail.com>
2025-07-09 19:27:10 +0545

    new feature added

---
+++ project01/src/app/random/randomFile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Button from '@/component/button'
+const RandomFile = () => {
+  return (
+    <div><Button props={'submit'}/></div>
+  )
+}
+
+export default RandomFile
---
+++ project01/src/component/button.tsx
@@ -0,0 +1,11 @@
+// import React, { ReactHTMLElement } from 'react
+
+const Button = (btton) => {
+  return (
+    <div>
+        <button type={btton.type}></button>
+    </div>
+  )
+}
+
+export default Button
